b19ad3201e83a10f76d6858ed700fa0b6d49f52c
Trevor Mackin <tismetrm@gmail.com>
2020-01-10 12:46:28 -0800

    Complete path to redirect the user to homepage on form submission

--- Bakery/Controllers/VendorsController.cs
+++ Bakery/Controllers/VendorsController.cs
@@ -13,17 +13,19 @@ namespace Bakery.Controllers
             return View();
         }
 
+
         [HttpGet("/vendors/new")]
         public ActionResult New()
         {
             return View();
         }
 
+
         [HttpPost("/vendors")]
         public ActionResult Create(string name, string description)
         {
             Vendor newVendor = new Vendor(name, description);
-            return RedirectToAction("Index");
+            return Redirect("/");
         }
        
     
