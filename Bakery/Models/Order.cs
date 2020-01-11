162129763af072c19b39e51900b507ca7288520a
Trevor Mackin <tismetrm@gmail.com>
2020-01-11 14:19:17 -0800

    Add date field to Order class Add date property to Order constructor Display date property in Orders Show.cshtml

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -1,37 +1,37 @@
 using System.Collections.Generic;
+using System;
+
 
 namespace Bakery.Models
 {
     public class Order
+  {
+    public string Title { get; set; } 
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    private static List<Order> _orders = new List<Order> {};
+
+    public Order(string title, string description, float price)
     {
-      public string Title { get; set; } 
-      public string Description { get; set; }
-      public float Price { get; set; }
-
-      // Add a Date field here 
-
-      public int Id { get; }
-      private static List<Order> _orders = new List<Order> {};
-
-      public Order(string title, string description, float price)
-      {
-          Title = title;
-          Description = description;
-          Price = price;
-          _orders.Add(this);
-          Id = _orders.Count;
-          // Add date to the constructor, and a date argument
-      }
-
-      public static Order Find(int searchId)
-      {
-        return _orders[searchId- 1];
-      }
-
-      public static List<Order> GetAll()
-      {
-        return _orders;
-      }
-
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = DateTime.Now.ToString("MM-dd-yyyy");
+      _orders.Add(this);
+      Id = _orders.Count;
     }
+    
+    public static Order Find(int searchId)
+    {
+      return _orders[searchId- 1];
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _orders;
+    }
+
+  }
 }
